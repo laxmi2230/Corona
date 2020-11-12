46a4a16dd0f157699863274292ffcf4633951f39
laxmi menon <46654095+laxmi2230@users.noreply.github.com>
2020-11-12 13:37:35 +0530

    Backend

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const {cloudinary} = require('./Utils/cloudinary');
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
+app.get('/api/images', async (req, res) => {
+    const { resources } = await cloudinary.search
+        .expression('folder:dev_setups')
+        .sort_by('public_id', 'desc')
+        .max_results(30)
+        .execute();
+
+    const publicIds = resources.map((file) => file.public_id);
+    res.send(publicIds);
+});
+
+app.post('/api/upload', async (req, res) => {
+    try {
+        const fileStr = req.body.data;
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: 'dev_setups',
+        });
+        console.log(uploadResponse);
+        res.json({ msg: 'yaya' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
+
+const port = process.env.PORT || 3001; 
+app.listen(port, () => {
+  console.log(`app is running on ${port}`);
+});
